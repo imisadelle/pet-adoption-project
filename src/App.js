9546689d54c9223a3a379d115fc542456916efdb
Isadelle Mercedes <Isadelle@gmail.com>
2020-04-05 19:29:38 -0400

    Home page jumbotron with starting css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
+// import 'bootstrap/dist/css/bootstrap.min.css'
 import Home from './pages/Home'
 import About from './pages/About'
 import Master from './pages/Master'
 import Details from './pages/Details'
 import Visit from './pages/Visit'
+// import Jumbotron from 'react-bootstrap/Jumbotron'
+// import Card from 'react-bootstrap/Card'
+// import Button from 'react-bootstrap/Button'
 
 function App() {
   return (
+    
     <div> 
     <Home/>
     <About/>
@@ -17,5 +22,7 @@ function App() {
     </div>
   );
 }
+
+
 
 export default App;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,36 @@
 import React, { Component } from 'react';
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Jumbotron from 'react-bootstrap/Jumbotron'
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
+import Navbar from 'react-bootstrap/Navbar'
 
-class Home extends Component {
-    render() {
+
+// class Home extends Component {
+//     render() {
+
+    function Home() {
         return (
-            <div>
+            <div className="Home">
+                <Navbar/>
+                <div className="container">
                 
-            </div>
-        );
-    }
-}
+                <Jumbotron>
+                    <h1>LOVABLE PAWS</h1>
+                    <p>
+                        Animals have love to give, and pets without homes even moreso.  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti porro qui, vel, nobis veritatis quas perferendis in incidunt eveniet possimus adipisci ex odio ab sapiente ducimus eaque? Id, tempora quam.
+                    </p>
+                    <p>
+                    <Button variant="primary">Learn more</Button>
+                    </p>
+                </Jumbotron>
+                </div>
+      
+             </div>
+            
+                );
+      }
+ 
 
 export default Home;
