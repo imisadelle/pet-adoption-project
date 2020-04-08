7678bcc5faf531a27d2886861a6f50376b86be67
darcipons <darcimarie33@gmail.com>
2020-04-07 20:29:09 -0400

    updated page

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -2,6 +2,9 @@ import React, { Component } from 'react'
 import axios from 'axios'
 import './Details.css'
 import Pug from '../images/pug.jpg'
+import Bone from '../images/bone.png'
+import Ball from '../images/ball.png'
+import Rope from '../images/rope.png'
 
 class Details extends Component {
 
@@ -25,28 +28,49 @@ class Details extends Component {
             <div className='page'>
               <div className='dog'>Dog Profile</div>
 
-              <div className='profile-container'>
-                <div className='left-container'>
-                  <div className='profile-pic-container'>
-                    <img src={Pug} alt='pug pic' className='profile-pic'></img>
+              <div className='center-container'>
+                <div className='profile-container'>
+                  <div className='left-container'>
+                    <div className='profile-pic-container'>
+                      <img src={Pug} alt='pug pic' className='profile-pic'></img>
+                    </div>
+
+                    <div className='bio'>
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. At consectetur lorem donec massa sapien faucibus et molestie."
+                    </div>
                   </div>
 
-                  <div className='bio'>
-                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. At consectetur lorem donec massa sapien faucibus et molestie."
+                  <div className='right-container'>
+                    <div className='dog-facts-container'>
+                      <div className='dog-facts-title'>DOGNAME is:</div>
+                      <div className='dog-facts'>Fun, Loving, Good with Cats, and Playful</div>
+                    </div>
+
+                    <div className='breed-facts-container'>
+                      <div className='breed-facts-title'>{this.state.name}s are:</div>
+                      <div className='breed-facts'>Bred for: {this.state.bred_for}</div>
+                      <div className='breed-facts'>Temperament: {this.state.temperament}</div>
+                      <div className='breed-facts'>Breed Group: {this.state.breed_group}</div>
+                    </div>
                   </div>
                 </div>
 
-                <div className='right-container'>
-                  <div className='dog-facts-container'>
-                    <div className='dog-facts-title'>DOGNAME is:</div>
-                    <div className='dog-facts'>Fun, Loving, Good with Cats, and Playful</div>
-                  </div>
+                <div className='products-container'>
+                  <div className='product-title'>Products</div>
 
-                  <div className='breed-facts-container'>
-                    <div className='breed-facts-title'>{this.state.name}s are:</div>
-                    <div className='breed-facts'>Bred for: {this.state.bred_for}</div>
-                    <div className='breed-facts'>Temperament: {this.state.temperament}</div>
-                    <div className='breed-facts'>Breed Group: {this.state.breed_group}</div>
+                  <div className='product-img-container'>
+                    <div className='product1'>
+                      <img src={Bone} alt='plastic bone' className='product'></img>
+                      <div>Bone</div>
+                    </div>
+                    <div className='product2'>
+                      <img src={Rope} alt='rope toy' className='product'></img>
+                      <div>Rope</div>
+                    </div>
+                    <div className='product3'>
+                      <img src={Ball} alt='plastic ball' className='product'></img>
+                      <div>Ball</div>
+                    </div>
                   </div>
                 </div>
               </div>
