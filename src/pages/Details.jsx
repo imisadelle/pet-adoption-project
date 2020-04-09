6a6f271de36a672fa8f17ecaca4703c1af0347a5
darcipons <darcimarie33@gmail.com>
2020-04-09 14:38:13 -0400

    added css

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -36,7 +36,7 @@ class Details extends Component {
                     </div>
 
                     <div className='bio'>
-                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. At consectetur lorem donec massa sapien faucibus et molestie."
+                    "Is your name Wifi? Cuz I'm feeling a connection"
                     </div>
                   </div>
 
