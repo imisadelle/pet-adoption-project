2eb158fb8fe7fc8be15feca56eb865ec7193b802
Isadelle Mercedes <Isadelle@gmail.com>
2020-04-12 23:11:23 -0400

    Add AboutUs content

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,21 +1,40 @@
 import React, { Component } from 'react';
 import {
+    Jumbotron,
     Col,
     Row,
-    Container
-  } from "react-bootstrap";
+    Container,
+    Image
+  } from 'react-bootstrap';
+  import './About.css';
+  import IM_profile from '../images/IM_profile.jpeg'
+  import darci_profile_pic from '../images/darci_profile_pic.jpg'
+
+ 
 
 class About extends Component {
     render() {
         return (
-            <Container>
+            <Container className="aboutUs">
+                <h1>Learning is a Journey</h1>
                 <div>
-                <h1>The History of Lovable Pets</h1>
+                <h2>The path to self-improvement led us to Wynwood</h2>
                 </div>
-                <div>
-                <h1>The Team behind Lovable Pets</h1>
-                </div>
-            </Container>
+            
+                <Container>
+                    <Row>
+                        <Col xs={6} md={4}>
+                        <Image src={ IM_profile } rounded fluid/>
+                        </Col>
+                        <Col xs={6} md={4}>
+                        <Image src={ darci_profile_pic } rounded fluid/>
+                        </Col>
+                        <Col xs={6} md={4}>
+                        <Image src="https://ca.slack-edge.com/T055GEHEJ-UN66Y5AJK-067dd872a453-512" rounded fluid/>
+                        </Col>
+                    </Row>
+                    </Container>
+                </Container>
         );
     }
 }
