db220a501ac57d5557063a099d04fe4ee86fc3e1
Alexia Panameno <alexiapanameno@gmail.com>
2020-04-13 21:03:25 -0400

    petlist filter changes

--- src/components/PetListCardUI.jsx
+++ src/components/PetListCardUI.jsx
@@ -1,22 +1,37 @@
 import React from "react";
+import { Card, Button } from "react-bootstrap";
+import "./PetListCards.css";
 
-const PetListCardUI = ({pet}) => {
+const PetListCardUI = ({ pet }) => {
   return (
-    <div className="card text-center">
-      <div className="overflow">
-        <img src="ball.npg" alt="" />
-      </div>
-      <div className="card-body text-dark">
-        <h4 className="card-title">Card Title</h4>
-        <p className="card-text text-secondary">
-          {pet.description}
-        </p>
-        <a href="#" className="btn btn-outline-success">
-          Go Anywhere
-        </a>
-      </div>
+    <div style={{ marginBottom: "2em" }}>
+      <Card style={{ width: "18rem" }}>
+        <Card.Img variant="top" src="src/images/ball.png" />
+        <Card.Body>
+          <Card.Title>{pet.name}</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+          <Button variant="primary">Pet Details</Button>
+        </Card.Body>
+      </Card>
     </div>
   );
 };
 
 export default PetListCardUI;
+
+{
+  /* <div className="overflow">
+        <img src="ball.npg" alt="" />
+      </div>
+      <div className="card-body text-dark">
+        <h4 className="card-title">{pet.name}</h4>
+        <img src="src/images/ball.png" alt="petlist-img" />
+        <p className="card-text text-secondary">{pet.description}</p>
+        <a href="#" className="btn btn-outline-success">
+          Pet Details
+        </a>
+      </div> */
+}
--- src/components/PetListFilter.jsx
+++ src/components/PetListFilter.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./PetListFilter.css";
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { value: "pitbull" };
+    this.state = {
+      breed: "",
+      age: "",
+      gender: "",
+      size: ""
+    };
 
     // this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -15,8 +21,11 @@ export default class App extends React.Component {
   //   }
 
   handleSubmit(event) {
-    alert("Your favorite flavor is: " + this.state.value);
+    // alert("Your chosen dog breed is: " + this.state.value);
     event.preventDefault();
+    // grab the values from the form and apply them to a fetch ("GET") request
+    ``;
+    // use this.setState({}) to set properties of state object
   }
 
   handleChange = event => {
@@ -26,18 +35,54 @@ export default class App extends React.Component {
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
-        <label>
-          Breed:
-          <select value={this.state.value} onChange={this.handleChange}>
-            <option value="grapefruit">Pitbull</option>
-            <option value="lime">Chihuahua</option>
-            <option value="coconut">Labradoodle</option>
-            <option value="mango">German Sheperd</option>
-          </select>
-        </label>
-        <input type="submit" value="Submit" />
+        <div class="pet-filter">
+          <div class="pet-filter">
+            <label>
+              Breed:
+              <select value={this.state.value} onChange={this.handleChange}>
+                <option value="null"></option>
+                <option value="Pitbull">Pitbull</option>
+                <option value="Chihuahua">Chihuahua</option>
+                <option value="Labradoodle">Labradoodle</option>
+                <option value="German Shepard">German Shepard</option>
+              </select>
+            </label>
+          </div>
+          <div class="pet-filter">
+            <label>
+              Gender:
+              <select value={this.state.value} onChange={this.handleChange}>
+                <option value="null"></option>
+                <option value="Male">Male</option>
+                <option value="Female">Female</option>
+              </select>
+            </label>
+          </div>
+          <div class="pet-filter">
+            <label>
+              Age:
+              <select value={this.state.value} onChange={this.handleChange}>
+                <option value="null"></option>
+                <option value="Puppy">Puppy</option>
+                <option value="Adult">Adult</option>
+                <option value="Senior">Senior</option>
+              </select>
+            </label>
+          </div>
+          <div class="pet-filter">
+            <label>
+              Size:
+              <select value={this.state.value} onChange={this.handleChange}>
+                <option value="null"></option>
+                <option value="Small">Small</option>
+                <option value="Medium">Medium</option>
+                <option value="Large">Large</option>
+              </select>
+            </label>
+          </div>
+          <input type="submit" value="Submit" class="petlist-btn" />
+        </div>
       </form>
     );
   }
 }
-
